c30ab3dc2b0fd8ae73da35511ff271ebff47e49b
Sayooj <sayoojsayoojks72@gmail.com>
2026-02-11 20:58:57 +0530

    Initial commit

---
+++ js/config.js
@@ -0,0 +1,13 @@
+const clientData = {
+  herName: "Meera",
+  boyfriendName: "Rahul",
+  profileImage: "assets/profile.jpg",
+  photos: [
+    "assets/img1.jpg",
+    "assets/img2.jpg",
+    "assets/img3.jpg"
+  ],
+  letterMessage: "Since the day I met you, my life has been a beautiful melody. You are the sunshine that brightens my darkest days and the stars that guide me home. Every moment with you is a treasure I hold dear. Getting to celebrate you today is the greatest gift of all. I love you more than words can say.",
+  birthdayDate: "March 15",
+  musicFile: "assets/music.mp3"
+};
